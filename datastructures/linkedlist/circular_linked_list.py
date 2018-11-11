0f99255a4695bce5cdbcb2bcea8db7c5759168a8
Caiden Pyle <40413115+Caidentp@users.noreply.github.com>
2018-11-11 16:01:31 -0600

    Update circular_linked_list.py

--- datastructures/linkedlist/circular_linked_list.py
+++ datastructures/linkedlist/circular_linked_list.py
@@ -10,7 +10,6 @@ class CircularLinkedList(AbstractLinkedList):
 
     def push(self, new_node_data):
         new_node = CNode(new_node_data)
-
         if self.head is None:
             self.tail = new_node
         else:
@@ -20,9 +19,7 @@ class CircularLinkedList(AbstractLinkedList):
         self.size += 1
 
     def append(self, new_node_data):
-
         new_node = CNode(new_node_data)
-
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -33,16 +30,13 @@ class CircularLinkedList(AbstractLinkedList):
         self.size += 1
 
     def insert(self, index, new_node_data):
-
         if index >= len(self) or self.head is None:
             raise IndexError('List index out of range.')
-
         if index == 0:
             self.push(new_node_data)
         else:
             new_node = CNode(new_node_data)
             temp = self.head
-
             for x in range(index-1):
                 temp = temp.next
             new_node.next = temp.next
@@ -50,10 +44,8 @@ class CircularLinkedList(AbstractLinkedList):
             self.size += 1
 
     def delete(self, index):
-
         if index >= len(self) or self.head is None:
             raise IndexError('List index out of range.')
-
         if index == 0:
             if self.head is self.tail:
                 self.head = None
@@ -64,7 +56,6 @@ class CircularLinkedList(AbstractLinkedList):
         else:
             temp = self.head
             previous = self.head
-
             for x in range(index):
                 if temp is self.tail:
                     break
