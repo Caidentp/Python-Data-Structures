34b0ffad733f222ee9e889c495609c183d603197
Caiden Pyle <40413115+Caidentp@users.noreply.github.com>
2018-11-11 16:02:51 -0600

    Update doubly_linked_list.py

--- datastructures/linkedlist/doubly_linked_list.py
+++ datastructures/linkedlist/doubly_linked_list.py
@@ -10,7 +10,6 @@ class DoublyLinkedList(AbstractLinkedList):
 
     def push(self, new_node_data):
         new_node = DNode(new_node_data)
-
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -22,7 +21,6 @@ class DoublyLinkedList(AbstractLinkedList):
 
     def append(self, new_node_data):
         new_node = DNode(new_node_data)
-
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -33,16 +31,13 @@ class DoublyLinkedList(AbstractLinkedList):
         self.size += 1
 
     def insert(self, index, new_node_data):
-
         if index >= len(self) or self.head is None:
             raise IndexError('List index out of range')
-
         if index == 0:
             self.push(new_node_data)
         else:
             new_node = DNode(new_node_data)
             temp = self.head
-
             for x in range(index-1):
                 temp = temp.next
 
@@ -55,7 +50,6 @@ class DoublyLinkedList(AbstractLinkedList):
     def delete(self, index):
         if index >= len(self) or self.head is None:
             raise IndexError('List index out of range.')
-
         if index == 0:
             if self.head is self.tail:
                 self.head = None
@@ -66,14 +60,13 @@ class DoublyLinkedList(AbstractLinkedList):
         else:
             temp = self.head
             previous = self.head
-
             for x in range(index):
                 if temp is self.tail:
                     break
                 previous = temp
                 temp = temp.next
-            previous.next = temp.next
 
+            previous.next = temp.next
             if temp is self.tail:
                 self.tail = previous
             else:
