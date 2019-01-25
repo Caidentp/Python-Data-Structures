efeac9b3ed9d153850dd679c17a92f46b7f23e9f
Pyle <v31353@txt.textron.com>
2019-01-25 12:31:00 -0600

    Added test wuite to graph

--- datastructures/graph/adjacency_list.py
+++ datastructures/graph/adjacency_list.py
@@ -1,5 +1,4 @@
 class Graph(object):
-
     def __init__(self):
         self.master_list = dict()
 
--- datastructures/graph/tests/test_graph.py
+++ datastructures/graph/tests/test_graph.py
@@ -8,8 +8,38 @@ from adjacency_list import Graph
 
 
 class TestSuite(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.graph = Graph()
 
+    def test_add_vertex_empty(self):
+        self.graph.add_vertex(1)
+        self.assertIn(1, self.graph)
+
+    def test_add_vertex_populated(self):
+        self.graph.add_vertex(2)
+        self.graph.add_vertex(3)
+        self.assertIn(2, self.graph)
+        self.assertIn(3, self.graph)
+
+    def test_add_edge_no_weight(self):
+        self.graph.add_edge(1, 2)
+        self.assertIn(2, self.graph[1])
+
+    def test_add_edge_with_weight(self):
+        self.graph.add_edge(2, 3, 10)
+        self.assertIn(3, self.graph[2])
+        self.assertEqual(10, self.graph[2][3])
+
+    def test_add_edge_invalid_first_arg(self):
+        self.graph.add_edge(4, 3)
+        self.assertIn(4, self.graph)
+        self.assertIn(3, self.graph[4])
+
+    def test_add_edge_invalid_second_arg(self):
+        self.graph.add_edge(4, 5)
+        self.assertIn(5, self.graph)
+        self.assertIn(5, self.graph[4])
+        
 
 if __name__ == '__main__':
     unittest.main()
